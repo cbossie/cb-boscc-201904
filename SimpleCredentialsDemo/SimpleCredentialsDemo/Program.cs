9e38c600d3560222d286566a5c864c2cf265a175
cbossie <cbossie@gmail.com>
2019-03-31 22:39:33 -0400

    Add simple Access

---
+++ SimpleCredentialsDemo/SimpleCredentialsDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCredentialsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate AWS Service Client
+            Amazon.S3.AmazonS3Client Cli = new Amazon.S3.AmazonS3Client();
+
+            // Configure Request
+            var blr = new ListBucketsRequest();
+
+            // Issue Request
+            var bucketResponse = Cli.ListBuckets(blr);
+
+            //Process Response
+            foreach(var b in bucketResponse.Buckets)
+            {
+                Console.WriteLine($"Bucket:{b.BucketName}, Created: {b.CreationDate.ToLongDateString()}");
+            }
+
+            Console.ReadKey();
+
+
+        }
+       
+    }
+}
